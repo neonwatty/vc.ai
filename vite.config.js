664314a30cc17d7b47778f5da4dd5c2d27a9af0a
Jeremy Watt <jermwatt@gmail.com>
2025-04-30 17:11:29 -0700

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // GitHub Pages deploys to a subdirectory that matches the repo name
-  // Ensuring all assets are loaded from https://neonwatty.github.io/vc.ai/
-  base: process.env.NODE_ENV === 'production' ? 'https://neonwatty.github.io/vc.ai/' : '/',
+  // Dynamic base path to support both development and GitHub Pages deployment
+  base: './'
 })
